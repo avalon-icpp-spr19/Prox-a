fd0e58b8d741a2fea51ced5b5b30133d586e2cf0
Vascodagamma <47292459+Vascodagamma@users.noreply.github.com>
2019-05-12 12:51:03 +0300

    Add files via upload

--- Date.hpp
+++ Date.hpp
@@ -1,3 +1,4 @@
+﻿#pragma once
 #pragma once
 #include <string>
 
@@ -38,46 +39,46 @@ namespace ext
 		int day;
 		Month month;
 		int year;
-		
-		
+
+
 	};
 
 	/*
-		Âîçâðàùàåò Þëèàíñêóþ äàòó
-		https://ru.wikipedia.org/wiki/%D0%AE%D0%BB%D0%B8%D0%B0%D0%BD%D1%81%D0%BA%D0%B0%D1%8F_%D0%B4%D0%B0%D1%82%D0%B0
-		ðàçäåë "Âû÷èñëåíèå íîìåðà þëèàíñêîãî äíÿ (JDN) ïî äàòå ãðèãîðèàíñêîãî êàëåíäàðÿ"
-		Òåñòîâûå äàííûå					Îæèäàåìûé ðåçóëüòàò
-		1.12.2018					2458454
-		1.1.2018					2458120
-		1.6.2000					2451697
-		21.12.2012					2456283
+	Âîçâðàùàåò Þëèàíñêóþ äàòó
+	https://ru.wikipedia.org/wiki/%D0%AE%D0%BB%D0%B8%D0%B0%D0%BD%D1%81%D0%BA%D0%B0%D1%8F_%D0%B4%D0%B0%D1%82%D0%B0
+	ðàçäåë "Âû÷èñëåíèå íîìåðà þëèàíñêîãî äíÿ (JDN) ïî äàòå ãðèãîðèàíñêîãî êàëåíäàðÿ"
+	Òåñòîâûå äàííûå					Îæèäàåìûé ðåçóëüòàò
+	1.12.2018					2458454
+	1.1.2018					2458120
+	1.6.2000					2451697
+	21.12.2012					2456283
 	*/
 	unsigned int countJND(Date date)
 	{
-		int  mo1, ye1, a, JDN;		
-	
+		int  mo1, ye1, a, JDN;
+
 		a = (14 - date.month) / 12;
 		ye1 = date.year + 4800 - a;
 		mo1 = date.month + 12 * a - 3;
 		JDN = date.day + ((153 * mo1 + 2) / 5) + (365 * ye1)
 			+ (ye1 / 4) - (ye1 / 100) + (ye1 / 400) - 32045;
-		std::cout << JDN << std::endl;
+	
 		return JDN;
 	}
 
 	/*
-		Ðàññ÷èòûâàåò êîëè÷åñòâî äíåé ìåæäó äâóìÿ äàòàìè.
-		Ïðè ðåàëèçâàöèè èñïîëüçóéòå CountJND
+	Ðàññ÷èòûâàåò êîëè÷åñòâî äíåé ìåæäó äâóìÿ äàòàìè.
+	Ïðè ðåàëèçâàöèè èñïîëüçóéòå CountJND
 	*/
 	int countDistance(Date from, Date to)
 	{
-		std::cout << "Between days:" << countJND(from) - countJND(to) << std::endl;
-		return 1;
+		
+		return countJND(from) - countJND(to);
 	};
 	/*
-		Âûâîäèò â êîíñîëü äàòó â ôîðìàòå: dd.mm.yyyy (2.10.2001)
-		Åñëè ïàðàìåòð DateFormat ðàâåí MonthAsString,
-		òî âûâåñòè äàòó â ôîðìàòå: dd monthName yyyy (2 îêòÿáðÿ 2001)
+	Âûâîäèò â êîíñîëü äàòó â ôîðìàòå: dd.mm.yyyy (2.10.2001)
+	Åñëè ïàðàìåòð DateFormat ðàâåí MonthAsString,
+	òî âûâåñòè äàòó â ôîðìàòå: dd monthName yyyy (2 îêòÿáðÿ 2001)
 	*/
 	void print(Date data, DateFormat format = DateFormat::MonthAsInt)
 	{
@@ -91,7 +92,7 @@ namespace ext
 			switch (data.month)
 			{
 			case Month::January:
-				std::cout << "January" << " " ;
+				std::cout << "January" << " ";
 				break;
 			case Month::February:
 				std::cout << "February" << " ";
@@ -137,96 +138,281 @@ namespace ext
 
 	};
 	/*
-		Âîçâðàùàåò ñåçîí (çèìà, âåñíà, ëåòî, îñåíü) ïåðåäàâàåìîé äàòû
+	Âîçâðàùàåò ñåçîí (çèìà, âåñíà, ëåòî, îñåíü) ïåðåäàâàåìîé äàòû
 	*/
-	int getSeason(Date date)
+	Season getSeason(Date date)
 	{
+		Season sizon;
 		switch (date.month)
 		{
 		case Month::January:
-			case Month::December:
+		case Month::December:
 		case Month::February:
-			std::cout << "winter" << " ";
+			sizon = Season::Winter;	
+		
 			break;
 		case Month::March:
-			
+
 		case Month::April:
-			
+
 		case Month::May:
-			std::cout << "spring" << " ";
+			sizon = Season::Spring;
 			break;
 		case Month::June:
-			
+
 		case Month::July:
-			
+
 		case Month::August:
-			std::cout << "summer" << " ";
+			sizon = Season::Summer;
 			break;
 		case Month::September:
-			
+
 		case Month::October:
-			
+
 		case Month::Novemver:
-			std::cout << "autumn" << " ";
+			sizon = Season::Autumn;
 			break;
-		
-			
+
+
 
 		default:
 			break;
 		}
-		cout << endl;
-		return 1;
+		
+		return sizon;
 	};
 
 	/*
-		Ñðàâíèâàåò äàòû íà ýêâèâàëåíòíîñòü
+	Ñðàâíèâàåò äàòû íà ýêâèâàëåíòíîñòü
 	*/
-	bool isSame(Date lhs, Date rhs)
+	void isSame(Date lhs, Date rhs)
 	{
 		bool ans = false;
 		if (lhs.day == rhs.day && lhs.month == rhs.month &&
 			lhs.year == rhs.year)ans = true;
-		std::cout << "isSame: "<< ans << std::endl;
-		return 1;
+		std::cout << "isSame: " << ans << std::endl;
 		
+
 	};
 
 	/*
-		Ìåíÿåò äàòû ìåñòàìè
+	Ìåíÿåò äàòû ìåñòàìè
 	*/
 	void swap(Date * lhs, Date * rhs)
 	{
+		Date horse;
+		Date *ptr = &horse;
+		*ptr = *lhs;
+		*lhs = *rhs;
+		*rhs = *ptr;
 		
 
-		
 	};
 
 	/*
-		Âîçâðàùàåò óêàçàòåëü íà áîëüøóþ äàòó
+	Âîçâðàùàåò óêàçàòåëü íà áîëüøóþ äàòó
 	*/
-	Date * max(Date * lhs, Date * rsh);
+	Date  *max(Date * lhs, Date * rsh)
+	{
+		
+	
+		if (countJND(*lhs) > countJND(*rsh))
+		{
+			return lhs;
+			
+		}
+		
+		else 
+		{
+			return rsh;
+		}
+		
+	}
 
 	/*
-		Âîçâðàùàåò óêàçàòåëü íà ìåíüøóþ äàòó
+	Âîçâðàùàåò óêàçàòåëü íà ìåíüøóþ äàòó
 	*/
-	Date * min(Date * lhs, Date * rsh);
+	Date * min(Date * lhs, Date * rsh)
+	{
+
+
+		if (countJND(*lhs) < countJND(*rsh))
+		{
+			return lhs;
+
+		}
+
+		else
+		{
+			return rsh;
+		}
+
+	}
+
 
 	/*
-		Âîçâðàùàåò óêàçàòåëü íà ìèíèìàëüíóþ äàòó â ïåðåäàííîì ìàññèâå äàò
+	Âîçâðàùàåò óêàçàòåëü íà ìèíèìàëüíóþ äàòó â ïåðåäàííîì ìàññèâå äàò
 	*/
-	Date * getMinDate(Date dates[], int size);
+	Date * getMinDate(Date dates[], int size)
+	{
+		Date min;
+		Date *ptr = &min;
+		for (int i = 0; i < size; ++i)
+		{
+			for (int j = 0; j < size; ++j)
+			{
+				if (countJND(dates[i]) < countJND(dates[j]) && i != j)
+				{
+					*ptr = dates[i];
+				}
+			}
+
+		}
+
+		return ptr;
+	}
 
 	/*
-		Âîçâðàùàåò óêàçàòåëü íà ìàêñèìàëüíóþ äàòó â ïåðåäàííîì ìàññèâå äàò
+	Âîçâðàùàåò óêàçàòåëü íà ìàêñèìàëüíóþ äàòó â ïåðåäàííîì ìàññèâå äàò
 	*/
-	Date * getMaxDate(Date dates[], int size);
+	Date * getMaxDate(Date dates[], int size)
+	{
+		Date max;
+		Date *ptr = &max;
+		for (int i = 0; i < size; ++i)
+		{
+			for (int j = 0; j < size; ++j)
+			{
+				if (countJND(dates[i]) > countJND(dates[j]) && i != j)
+				{
+					*ptr = dates[i];
+				}
+			}
+
+		}
+
+		return ptr;
+	}
 
 	/*
-		Ïðîâåðÿåò ìîæåò ëè ñóùåñòâîâàòü äàòà ñ ïåðåäàâàåìûìè çíà÷åíèÿìè.
-		Â ñëó÷àå óñïåõà âîçâðàùàåò true, è çàïîëíÿåò äàòó ïî ïåðåäàííîìó àäðåñó.
-		Åñëè äàòà ñ ïåðåäàííûìè ïàðàìåòðàìè ñóùåñòâîâàòü íå ìîæåò,
-		âîçâðàùàåò false.
+	Ïðîâåðÿåò ìîæåò ëè ñóùåñòâîâàòü äàòà ñ ïåðåäàâàåìûìè çíà÷åíèÿìè.
+	Â ñëó÷àå óñïåõà âîçâðàùàåò true, è çàïîëíÿåò äàòó ïî ïåðåäàííîìó àäðåñó.
+	Åñëè äàòà ñ ïåðåäàííûìè ïàðàìåòðàìè ñóùåñòâîâàòü íå ìîæåò,
+	âîçâðàùàåò false.
 	*/
-	bool tryFillDate(Date * date, int year, int month, int day);
+	bool tryFillDate(Date * date, int y, int m, int d)
+	{
+		bool ans = false;
+		if (d < 32 && m < 13)
+		{
+			switch (m)
+			{
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				if (d <= 31)
+				{
+					ans = true;
+
+				}
+				else
+				{
+					ans = false;
+				}
+				break;
+			case 2:
+				if ((y % 4 == 0 && (y % 100 != 0) || (y % 400 == 0))
+					&& (d <= 29))
+				{
+					ans = true;
+				}
+				else if (((y % 4 != 0 && (y % 100 == 0)) || (y % 400 != 0))
+					&& (d <= 28))
+				{
+					ans = true;
+				}
+				else
+				{
+					ans = false;
+				}
+				break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				if (d <= 30)
+				{
+					ans = true;
+				}
+				else
+				{
+					ans = false;
+				}
+
+			default:
+				break;
+			}
+		}
+		else
+		{
+			ans = false;
+		}
+		if (ans == true)
+		{
+			date->day = d;			
+			date->year = y;
+			Month loop;
+			switch (m)
+				{
+				case 1:
+					loop = Month::January;
+					break;
+				case 2:
+					loop = Month::February;
+					break;
+				case 3:
+					loop = Month::March;
+					break;
+				case 4:
+					loop = Month::April;
+					break;
+				case 5:
+					loop = Month::May;
+					break;
+				case 6:
+					loop = Month::June;
+				case 7:
+					loop = Month::July;
+					break;
+				case 8:
+					loop = Month::August;
+					break;
+				case 9:
+					loop = Month::September;
+					break;
+				case 10:
+					loop = Month::October;
+					break;
+				case 11:
+					loop = Month::Novemver;
+					break;
+				case 12:
+					loop = Month::December;
+					break;
+				default:
+					break;
+				}
+			date->month = loop;
+			return ans;
+		
+	}
+		else
+		{
+			return ans;
+		}
+	}
 }
